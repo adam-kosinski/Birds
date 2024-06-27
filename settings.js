84839e5108a71ae6097fcc497f010c4c32ef396e
Adam <adamkosinski631@gmail.cm>
2024-06-27 01:56:52 -0400

    allow-all-taxa setting uses localstorage

--- settings.js
+++ settings.js
@@ -6,13 +6,33 @@ document.querySelectorAll(".settings-button").forEach(button => {
 document.getElementById("close-settings").addEventListener("click", () => {
     document.getElementById("settings-modal").style.display = "none";
 });
-document.getElementById("allow-searching-all-taxa").addEventListener("change", (e) => {
+document.getElementById("allow-all-taxa").addEventListener("change", (e) => {
+    localStorage.setItem("setting:allow-all-taxa", e.target.checked);
     updateSearchFiltering(e.target.checked);
 });
 
 
 
-function updateSearchFiltering(allow_all_taxa){
+function updateSearchFiltering(allow_all_taxa) {
     only_search_for_birds = !allow_all_taxa;  // see add_remove_birds.js
     document.getElementById("add-bird-input").placeholder = `Search for ${allow_all_taxa ? 'taxon' : 'bird'} to add`
 }
+
+
+
+// load from local storage
+function loadLocalStorage() {
+    let allow_all_taxa;
+    switch (localStorage.getItem("setting:allow-all-taxa")) {
+        case "true": allow_all_taxa = true; break;
+        case "false": allow_all_taxa = false; break;
+        case null:
+            console.log("Setting initial allow_all_taxa=false");
+            allow_all_taxa = false; // default value  
+            localStorage.setItem("setting:allow-all-taxa", allow_all_taxa);
+    }
+    document.getElementById("allow-all-taxa").checked = allow_all_taxa;
+    updateSearchFiltering(allow_all_taxa);
+}
+
+loadLocalStorage();
