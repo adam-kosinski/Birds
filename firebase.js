f2ba75f07158b279c0b91cd9e27d32a6cc879923
Adam <adamkosinski631@gmail.cm>
2024-07-03 03:45:09 -0400

    Reading and writing skipped ids works, including server reconciliation

--- firebase.js
+++ firebase.js
@@ -19,9 +19,49 @@ test_ref.get().then(doc => {
     }
 })
 
+
+
+// method for using:
+// when a game starts, get the skipped ids and store in a variable
+// as the user plays the game, add ids to this object
+// when the game ends or before the page unloads, update the database with this object
+// this way we only have to do one write
+// BTW we will need to get skipped ids again right before writing in case someone else changed it
+
+
 async function getSkippedIds(taxa, mode) {
     const collection = db.collection(`skipped-${mode}`);
+    const skipped_ids = {}; // key is taxon id, value is array of iNaturalist observation ids
+    const promises = [];
     taxa.forEach(taxon_id => {
-        
+        promises.push(
+            collection.doc(String(taxon_id)).get().then(doc => {
+                skipped_ids[taxon_id] = doc.exists ? doc.data().ids : [];
+            })
+        )
     });
+    await Promise.all(promises);
+    return skipped_ids;
+}
+
+
+async function setSkippedIds(skipped_ids, mode) {
+    // skipped_ids is an object, key is taxon id, value is array of iNaturalist observation ids
+
+    // get data again, in case it changed
+    const firebase_skipped_ids = await getSkippedIds(Object.keys(skipped_ids), mode);
+
+    // reconcile and write
+    const collection = db.collection(`skipped-${mode}`);
+    for (let [taxon_id, ids] of Object.entries(skipped_ids)) {
+        // remove any duplicates
+        const set = new Set(ids);
+        ids = Array.from(set);
+
+        // add any new ids from firebase to my id list
+        firebase_skipped_ids[taxon_id].forEach(id => { if (!set.has(id)) ids.push(id) });
+
+        // write
+        collection.doc(String(taxon_id)).set({ ids: ids });
+    }
 }
