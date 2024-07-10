f89e98fd7b123dd6fdeb75339cdc7d3c73114335
Adam <adamkosinski631@gmail.cm>
2024-07-10 18:46:03 -0400

    Added odonata preset

--- config.js
+++ config.js
@@ -55,12 +55,13 @@ function getInfoURL(taxon_obj) {
     }
     //Amphibians and Reptiles
     if ((taxon_obj.ancestor_ids.includes(20978) || taxon_obj.ancestor_ids.includes(26036)) && taxon_obj.rank_level <= 10) {  // rank level 10 is species, 5 is subspecies
-        if(taxon_obj.id === 73887) return "https://herpsofnc.org/corn-snake/"  // herps of nc has a typo for the scientific name for corn snakes
-        if(taxon_obj.id === 788326) return "https://herpsofnc.org/eastern-ribbon-snake/" // herps of nc has a typo for eastern ribbon snake too
+        if (taxon_obj.id === 73887) return "https://herpsofnc.org/corn-snake/"  // herps of nc has a typo for the scientific name for corn snakes
+        if (taxon_obj.id === 788326) return "https://herpsofnc.org/eastern-ribbon-snake/" // herps of nc has a typo for eastern ribbon snake too
         return "https://herpsofnc.org/?s=" + taxon_obj.name.split(" ").slice(0, 2).join("+");  // only include genus and species, not subspecies (can mess up search)
     }
     //Insect orders
     let bugguide_mapping = {
+        // insect orders
         47157: 57,    //Butterflies and Moths
         47208: 60,    //Beetles
         47201: 59,    //Ants, Bees, Wasps, and Sawflies
@@ -79,9 +80,46 @@ function getInfoURL(taxon_obj) {
         47504: 76,    //Stoneflies
         47864: 233428,    //Alderflies, Dobsonflies, and Fishflies
         49369: 56,    //Scorpionflies, Hangingflies, and Allies
-        48301: 79    //Silverfishes
+        48301: 79,    //Silverfishes
+
+        // dragonflies and damselflies
+        84549: 603,    //Common Whitetail
+        61495: 577,    //Eastern Pondhawk
+        59774: 598,    //Blue Dasher
+        84481: 601,    //Ebony Jewelwing
+        104586: 576,    //Great Blue Skimmer
+        103498: 604,    //Fragile Forktail
+        104580: 2650,    //Slaty Skimmer
+        47934: 586,    //Widow Skimmer
+        59776: 8058,    //Eastern Amberwing
+        47975: 2642,    //Blue-fronted Dancer
+        // 103927: 27364,    //Blue Corporal  - wikipedia better
+        94575: 14911,    //Blue-tipped Dancer
+        94545: 584,    //Variable Dancer
+        50147: 2591,    //Halloween Pennant
+        100034: 2584,    //Swamp Darner
+        63160: 595,    //Calico Pennant
+        100212: 10646,    //Common Baskettail
+        68229: 3828,    //Carolina Saddlebags
+        520958: 3148,    //Lancet Clubtail
+        67731: 585,    //Common Green Darner
+        // 94557: 2640,    //Powdered Dancer  - wikipedia better
+        104575: 600,    //Spangled Skimmer
+        96907: 594,    //Banded Pennant
+        520951: 17841,    //Ashy Clubtail
+        110625: 573,    //Common Sanddragon
+        99897: 596,    //Familiar Bluet
+        103494: 597,    //Citrine Forktail
+        99609: 6388,    //Black-shouldered Spinyleg
+        94571: 2641,    //Blue-ringed Dancer
+        99924: 583,    //Orange Bluet
+        99283: 589,    //Stream Cruiser
+        100403: 590,    //Little Blue Dragonlet
+        // 102006: 17837,    //Dragonhunter  - wikipedia better
+        104585: 4085,    //Painted Skimmer
+        // 108344: 8933    //Wandering Glider  - wikipedia better
     }
-    if (taxon_obj.ancestor_ids.includes(47158) && taxon_obj.rank == "order") {
+    if (taxon_obj.id in bugguide_mapping) {
         return "https://bugguide.net/node/view/" + bugguide_mapping[taxon_obj.id];
     }
 
@@ -113,7 +151,7 @@ function getInfoURL(taxon_obj) {
     if (taxon_obj.id === 1363728) return "https://en.wikipedia.org/wiki/Grouper";
     if (taxon_obj.id === 1359791) return "https://en.wikipedia.org/wiki/Anthiinae";
 
-    //Final default is the iNaturalist page for this taxa, which must exist
+    //Final default is the iNaturalist page for this taxon, which must exist
     return "https://www.inaturalist.org/taxa/" + taxon_obj.id;
 }
 
@@ -143,6 +181,12 @@ function getQuestionHTML(mode, taxon_obj, is_squirrel_intruder = false) {
         }
     }
 }
+
+
+/*
+For generating preset lists from list_taxa
+console.log(list_taxa.map(obj => `${obj.id},   //${obj.preferred_common_name}`).join(""))
+*/
 
 
 const PRESETS = {
@@ -596,6 +640,48 @@ const PRESETS = {
             47864,    //Alderflies, Dobsonflies, and Fishflies
             49369,    //Scorpionflies, Hangingflies, and Allies
             48301    //Silverfishes
+        ]
+    },
+    "Dragonflies and Damselflies (NC)": {
+        description: "Ah, dragons, damsels, knights in shining armor, how romantic... oh wait these are the insects.",
+        photo: "images/preset_odonata.jpg",
+        mode: "visual_id",
+        taxa: [
+            84549,   //Common Whitetail
+            61495,   //Eastern Pondhawk
+            59774,   //Blue Dasher
+            84481,   //Ebony Jewelwing
+            104586,   //Great Blue Skimmer
+            103498,   //Fragile Forktail
+            104580,   //Slaty Skimmer
+            47934,   //Widow Skimmer
+            59776,   //Eastern Amberwing
+            47975,   //Blue-fronted Dancer
+            103927,   //Blue Corporal
+            94575,   //Blue-tipped Dancer
+            94545,   //Variable Dancer
+            50147,   //Halloween Pennant
+            100034,   //Swamp Darner
+            63160,   //Calico Pennant
+            100212,   //Common Baskettail
+            68229,   //Carolina Saddlebags
+            520958,   //Lancet Clubtail
+            67731,   //Common Green Darner
+            94557,   //Powdered Dancer
+            104575,   //Spangled Skimmer
+            96907,   //Banded Pennant
+            520951,   //Ashy Clubtail
+            110625,   //Common Sanddragon
+            99897,   //Familiar Bluet
+            103494,   //Citrine Forktail
+            99609,   //Black-shouldered Spinyleg
+            94571,   //Blue-ringed Dancer
+            99924,   //Orange Bluet
+            99283,   //Stream Cruiser
+            100403,   //Little Blue Dragonlet
+            102006,   //Dragonhunter
+            104585,   //Painted Skimmer
+            108344   //Wandering Glider
         ]
     },
     "Common Piedmont Fungi and Lichens (NC)": {
