f482e0c22c76b87227327819f44430e3a9f4aaf6
Adam <adamkosinski631@gmail.cm>
2025-03-06 19:56:44 -0500

    hacky fix for spectrogram alignment on iOS

--- spectrogram.js
+++ spectrogram.js
@@ -1,28 +1,38 @@
 initSpectrogram();
 
 function initSpectrogram() {
+  if (data_source !== "ebird_calls") return;
+
   const audio = document.getElementById("birdsong-audio-0");
   const imgs_div = document.getElementById("spectrogram-images");
 
   // spectrogram scrolling
   let stopScroll = () => {};
-  audio.addEventListener("play", () => {
-    stopScroll();
-    stopScroll = scrollSpectrogram();
-  });
+
   audio.addEventListener("pause", () => {
     stopScroll();
   });
-  audio.addEventListener("seeking", () =>
-    updateSpectrogramPosition(audio.currentTime)
-  );
+  audio.addEventListener("seeking", () => {
+    stopScroll(true);
+    updateSpectrogramPosition(audio.currentTime);
+  });
 
   audio.addEventListener("loadstart", () => {
     // clean up from last time
     imgs_div.innerHTML = "";
+    stopScroll(true);
   });
+
   audio.addEventListener("timeupdate", () => {
-    if (!current.is_squirrel_intruder) {
+    // start audio on a timeupdate event, because this is the only reliable way to sync animation on iOS,
+    // which starts actually playing the audio some undetermined amount of time after the "play" event fires
+    // try not to activate this multiple times? detect if already scrolling?
+    if (!audio.paused) {
+      stopScroll(true); // just in case
+      stopScroll = scrollSpectrogram();
+    }
+
+    if (current && !current.is_squirrel_intruder) {
       // fetch spectrogram images as needed
       const t_buffered = Math.min(
         audio.duration,
@@ -58,24 +68,30 @@ function updateSpectrogramPosition(sec) {
 
 function scrollSpectrogram() {
   // starts spectrogram scrolling, returns function to call to stop the scrolling
-  // need to animate scrolling start/stop based on audio events rather than the timeupdate event,
-  // because the timeupdate event doesn't happen frequently enough
+  // by default, scrolling will stop at the next audio.currentTime spot (every 0.25 sec on avg)
+  // because when scrolling resumes, it will be from that spot, and we want to avoid a jump
+  // it's also possible to tell the scrolling to stop immediately
+
+  const audio = document.getElementById("birdsong-audio-0");
 
   const time0 = document.timeline.currentTime;
-  const audioStartPos = document.getElementById("birdsong-audio-0").currentTime;
-  let shouldStop = false;
+  const audioStartPos = audio.currentTime;
+
+  let timeToStop = Infinity;
+  let stopImmediately = false;
 
   const callback = (time) => {
     const elapsed_sec = (time - time0) / 1000;
     updateSpectrogramPosition(audioStartPos + elapsed_sec);
-    if (!shouldStop) {
+    if (audioStartPos + elapsed_sec < timeToStop && !stopImmediately) {
       requestAnimationFrame(callback);
     }
   };
 
   requestAnimationFrame(callback);
 
-  return () => {
-    shouldStop = true;
+  return (stopNow = false) => {
+    timeToStop = audio.currentTime + 0.25; //next approx currentTime position
+    stopImmediately = stopNow;
   };
 }
