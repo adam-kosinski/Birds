8ff033fd02f3c7214d7a71a7352a2767a62348ab
Adam <adamkosinski631@gmail.cm>
2025-05-13 13:30:25 -0400

    error handling if ip address fails (which it is over https)

--- user_location.js
+++ user_location.js
@@ -3,7 +3,7 @@ async function getIPLocation() {
   const ipData = await ipRes.json();
   const ip = ipData.ip;
 
-  const geoRes = await fetch(`https://ip-api.com/json/${ip}`);
+  const geoRes = await fetch(`http://ip-api.com/json/${ip}`);
   const geoData = await geoRes.json();
 
   const lat = geoData.lat;
@@ -26,18 +26,22 @@ async function getIPLocation() {
 }
 
 async function speciesCountsInLocation(lat, lng, taxonIds) {
-  const res = await fetch(
-    `https://api.inaturalist.org/v1/observations/species_counts?` +
-      `lat=${lat}&lng=${lng}&radius=${SPECIES_COUNTS_RADIUS}` +
-      `&taxon_id=${taxonIds.join(",")}&verifiable=true&per_page=100`
-  );
-  const data = await res.json();
   const counts = {};
   const readableCounts = new Map();
-  data.results.forEach((obj) => {
-    counts[obj.taxon.id] = obj.count;
-    readableCounts.set(obj.taxon.preferred_common_name, obj.count);
-  });
-  console.log(readableCounts);
+
+  if (lat && lng) {
+    const res = await fetch(
+      `https://api.inaturalist.org/v1/observations/species_counts?` +
+        `lat=${lat}&lng=${lng}&radius=${SPECIES_COUNTS_RADIUS}` +
+        `&taxon_id=${taxonIds.join(",")}&verifiable=true&per_page=100`
+    );
+    const data = await res.json();
+    data.results.forEach((obj) => {
+      counts[obj.taxon.id] = obj.count;
+      readableCounts.set(obj.taxon.preferred_common_name, obj.count);
+    });
+  }
+
+  console.log("counts", readableCounts);
   return counts;
 }
